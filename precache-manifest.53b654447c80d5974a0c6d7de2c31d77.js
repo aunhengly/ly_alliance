f317f51b930ce96b2c6b41f24a5e8ecc8302a8c2
Hengly Aun <aunhengly@gmail.com>
2022-04-24 15:39:13 -0700

    Updates

---
+++ precache-manifest.53b654447c80d5974a0c6d7de2c31d77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f530dca3bbc41db7e65e",
+    "url": "/ly_alliance/static/css/main.694d4644.chunk.css"
+  },
+  {
+    "revision": "f530dca3bbc41db7e65e",
+    "url": "/ly_alliance/static/js/main.f530dca3.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/ly_alliance/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "3b6de6c90bc25f09c7da",
+    "url": "/ly_alliance/static/css/2.357da1f5.chunk.css"
+  },
+  {
+    "revision": "3b6de6c90bc25f09c7da",
+    "url": "/ly_alliance/static/js/2.3b6de6c9.chunk.js"
+  },
+  {
+    "revision": "f9daf6b9a991839bfb13",
+    "url": "/ly_alliance/static/js/runtime~main.f9daf6b9.js"
+  },
+  {
+    "revision": "517170aab80ebdccd74399bcd392ca38",
+    "url": "/ly_alliance/static/media/cambogia.517170aa.jpg"
+  },
+  {
+    "revision": "a8ed3f66ae39913b39ee7b823b0ae9f6",
+    "url": "/ly_alliance/index.html"
+  }
+];
